8e3b01c0de388a7eb736e49cca4dea8380b7e069
Ilia Kalendarev <piknik0711@yandex.ru>
2023-09-19 18:18:57 +0300

    Initial commit

---
+++ src/main/java/com/pet/sumpro/SumproApplication.java
@@ -0,0 +1,13 @@
+package com.pet.sumpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SumproApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SumproApplication.class, args);
+    }
+
+}
